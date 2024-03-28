b0153bd04a5b13bb41433385d2fdc195d8e77704
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-03-28 20:26:29 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 42,
         color: '#010101'
       }}
     >
