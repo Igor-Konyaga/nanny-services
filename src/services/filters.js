12009584208841108b507b8916d357833f9c0e26
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-05-06 12:55:14 +0300

    update filtration sort population

--- src/services/filters.js
+++ src/services/filters.js
@@ -10,33 +10,33 @@ export const filtrationData = (filtrationCategory, nanniesData) => {
     }
     return 0;
   });
+  if (filtrationCategory === 'A to Z' || filtrationCategory === 'Z to A') {
+    return sortAB;
+  }
 
   const filterLessGreatThen = [...nanniesData].filter(nanny => {
     return filtrationCategory === 'Less than 10$'
       ? nanny.price_per_hour <= 10
       : nanny.price_per_hour >= 10;
   });
-
-  const filterIsPopular = [...nanniesData].filter(nanny => {
-    return filtrationCategory === 'Popular'
-      ? nanny.rating > 4
-      : nanny.rating <= 4;
-  });
-
-  if (filtrationCategory === 'A to Z' || filtrationCategory === 'Z to A') {
-    return sortAB;
-  }
   if (
     filtrationCategory === 'Less than 10$' ||
     filtrationCategory === 'Greater than 10$'
   ) {
     return filterLessGreatThen;
   }
+
+  const filterIsPopular = [...nanniesData].filter(nanny => {
+    const res =
+      filtrationCategory === 'Popular' ? nanny.rating > 4 : nanny.rating <= 4;
+    return res;
+  });
+
   if (
     filtrationCategory === 'Popular' ||
     filtrationCategory === 'Not popular'
   ) {
-    return filterIsPopular;
+    return filterIsPopular.sort((a, b) => b.rating - a.rating);
   }
 
   return nanniesData;
